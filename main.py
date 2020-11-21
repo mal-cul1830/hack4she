bea2e92ab550a4588f57b23acb68b8025591dfde
Malavika <malavikau1801@gmail.com>
2020-11-21 20:29:22 +0530

    adding news page

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import http.server
+import socketserver
+PORT = 8000
+Handler = http.server.CGIHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.serve_forever()
+
 def train_model():
     import pandas as pd
     from sklearn.ensemble import RandomForestClassifier
@@ -19,3 +26,23 @@ def train_model():
     score = accuracy_score(y, y_pred)
     print("Accuracy:",score)
     pickle.dump(clf, open('model.pkl','wb'))
+def get_form():
+    #!C:\Python27\python.exe
+    # Import modules for CGI handling
+    import cgi, cgitb
+    # Create instance of FieldStorage
+    form = cgi.FieldStorage()
+    # Get data from fields
+    first_name = form.getvalue('height')
+    last_name  = form.getvalue('weight')
+    print("Content-type:text/html")
+    print("")
+    print("")
+    print("Hello - Second CGI Program")
+    print("")
+    print("")
+    print("Hello %s %s" % (first_name, last_name))
+    print("")
+    print("")
+
+get_form()
